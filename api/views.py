afde04b13df5cf9d08d4f6c6dd8c40bbf336e232
Cameron <cameron_2000@live.co.uk>
2025-05-23 16:46:03 +0100

    add first trolley views

--- api/views.py
+++ api/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics, permissions
+from .models import Trolley, FrontLabel, BackLabel
+from .serializers import TrolleySerializer, FrontLabelSerializer, BackLabelSerializer
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Hello World from the API!!")
+class TrolleyView(generics.ListCreateAPIView):
+    queryset = Trolley.objects.all()
+    serializer_class = TrolleySerializer
+    permission_classes = [permissions.IsAuthenticated]  # adjust permissions as needed
+
+    def get_queryset(self):
+        # You can customize filtering here if needed
+        return super().get_queryset()
+
+
+class TrolleyInfo(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Trolley.objects.all()
+    serializer_class = TrolleySerializer
+    permission_classes = [permissions.IsAuthenticated]
