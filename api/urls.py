4f28b3a57bc76398784c5b682db39704c99defe3
Cameron <cameron_2000@live.co.uk>
2025-05-23 17:01:55 +0100

    create label views and urls

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import TrolleyView, TrolleyInfo
+from .views import TrolleyView, TrolleyInfo, FrontLabelListCreateAPIView, BackLabelListCreateAPIView
 
 urlpatterns = [
     path('trolleys/', TrolleyView.as_view(), name='trolley-list-create'),
     path('trolleys/<int:pk>/', TrolleyInfo.as_view(), name='trolley-detail'),
+    path('trolleys/<int:trolley_id>/front-labels/', FrontLabelListCreateAPIView.as_view(), name='frontlabel-list-create'),
+    path('trolleys/<int:trolley_id>/back-labels/', BackLabelListCreateAPIView.as_view(), name='backlabel-list-create'),
 ]
--- api/views.py
+++ api/views.py
@@ -18,3 +18,28 @@ class TrolleyInfo(generics.RetrieveUpdateDestroyAPIView):
     queryset = Trolley.objects.all()
     serializer_class = TrolleySerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class FrontLabelListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = FrontLabelSerializer
+
+    def get_queryset(self):
+        trolley_id = self.kwargs['trolley_id']
+        return FrontLabel.objects.filter(trolley_id=trolley_id)
+
+    def perform_create(self, serializer):
+        trolley_id = self.kwargs['trolley_id']
+        # Optionally, add extra validation if trolley exists etc.
+        serializer.save(trolley_id=trolley_id)
+
+
+class BackLabelListCreateAPIView(generics.ListCreateAPIView):
+    serializer_class = BackLabelSerializer
+
+    def get_queryset(self):
+        trolley_id = self.kwargs['trolley_id']
+        return BackLabel.objects.filter(trolley_id=trolley_id)
+
+    def perform_create(self, serializer):
+        trolley_id = self.kwargs['trolley_id']
+        serializer.save(trolley_id=trolley_id)
