e12c978fcc7e04bc2dc0c514f99c2118dfd15db1
Cameron <cameron_2000@live.co.uk>
2025-05-24 14:38:06 +0100

    create pinned views

--- pinned/views.py
+++ pinned/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Pinned
+from .serializers import PinnedSerializer
 
 # Create your views here.
+
+class PinnedList(generics.ListCreateAPIView):
+    permission_classes = permissions.IsAuthenticatedOrReadOnly
+    queryset = Pinned.objects.all()
+    serializer_class = PinnedSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+
+class PinnedDetail(generics.RetrieveDestroyAPIView):
+    permission_classes = permissions.IsAuthenticated
+    serializer_class = PinnedSerializer
+    queryset = Pinned.objects.all()
