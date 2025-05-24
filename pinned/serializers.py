bf01caaff40a47eee8f015850ba1761dba20e7e9
Cameron <cameron_2000@live.co.uk>
2025-05-24 14:33:46 +0100

    create pinned serializers.py

---
+++ pinned/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.db import IntegrityError
+from .models import Pinned
+
+class PinnedSerializer(serializers.Serializer):
+    creator = serializers.ReadOnlyField(source='creator.username')
+
+    class Meta:
+        model = Pinned
+        fields = ['id', 'creator', 'trolley', 'created_at']
+
+    def create(self, validated_data):
+        try:
+            return super().create(validated_data)
+        except IntegrityError:
+            raise serializers.ValidationError({
+                'detail': 'possible duplicate'
+            })
