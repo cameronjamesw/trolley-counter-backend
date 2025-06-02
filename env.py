8c92e971299271b69f8c1816cf0a4525244428c1
Cameron <cameron_2000@live.co.uk>
2025-06-02 18:12:58 +0100

    Untrack env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.environ["SECRET_KEY"] = "923ddb810813bb77c02a4b7d87943c30"
-
-os.environ["DATABASE_URL"] = "postgresql://counterdb_owner:npg_hwvBHi7D3blQ@ep-lingering-resonance-abyhwz6x-pooler.eu-west-2.aws.neon.tech/counterdb?sslmode=require"
