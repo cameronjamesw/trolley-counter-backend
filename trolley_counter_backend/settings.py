057ac72abe4e3bfad7e7bd2c5264e58c252e49c3
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-15 18:24:50 -0400

    correct python indent

--- trolley_counter_backend/settings.py
+++ trolley_counter_backend/settings.py
@@ -57,7 +57,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-   'localhost',
+    'localhost',
     '127.0.0.1',
     'trolley-counter-backend.onrender.com',
     'trolley-counter-frontend.onrender.com',
