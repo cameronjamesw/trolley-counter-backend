b8f146098f756b8fbe001e5218d35947f5a82cee
Cameron Wheatley <cameron_2000@live.co.uk>
2026-09-15 18:42:12 -0400

    update cors and allowed_hosts

--- trolley_counter_backend/settings.py
+++ trolley_counter_backend/settings.py
@@ -57,32 +57,29 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'trolley-counter-backend.onrender.com',
-    'trolley-counter-frontend.onrender.com',
+    "localhost",
+    "127.0.0.1",
+    "trolley-counter-backend.onrender.com",
+    "trolley-counter-frontend.onrender.com",
     os.environ.get("ALLOWED_HOST"),
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000",
+    "http://localhost:3000",
     "http://127.0.0.1:3000",
-    'https://trolley-counter-backend.onrender.com',
-    'https://trolley-counter-frontend.onrender.com',
+    "https://trolley-counter-frontend.onrender.com",
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 CSRF_TRUSTED_ORIGINS = [
-    'http://127.0.0.1:8000',
-    'http://localhost:3000',
-    'https://trolley-counter-backend.onrender.com',
-    'https://trolley-counter-frontend.onrender.com',
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://trolley-counter-frontend.onrender.com",
 ]
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS.append(os.environ.get('CLIENT_ORIGIN'))
-
-
-CORS_ALLOW_CREDENTIALS = True
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_LOGIN_METHODS = {'username'}
